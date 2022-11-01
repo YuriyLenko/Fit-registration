947d82efa4b2d2425e300f31e6ab13408414fb24
Yuriy Lenko <yura.lenko19@gmail.com>
2022-11-01 17:03:46 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function validateData() {
+  const nameEl = document.getElementById("name");
+  const ageEl = document.getElementById("age");
+  const weightEl = document.getElementById("weight");
+  const growthEl = document.getElementById("growth");
+
+  const name = nameEl.value;
+  const age = ageEl.value;
+  const weight = weightEl.value;
+  const growth = growthEl.value;
+
+  if (age < 18 || age > 60) {
+    alert("Вибачте, здається ви неправильно вказали ваш вік");
+  } else if (weight < 50 || weight > 120) {
+    alert("Вибачте, здається ви неправильно вказали вашу вагу");
+  } else if (growth < 140 || growth > 200) {
+    alert("Вибачте, здається ви неправильно вказали ваш зріст");
+  } else {
+    alert("Вітаємо! Ви успізхїшно створили обліковий запис");
+    nameEl.value = "";
+    ageEl.value = "";
+    weightEl.value = "";
+    growthEl.value = "";
+  }
+}
